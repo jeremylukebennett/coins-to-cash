1cbfc97c262c0f697486d878f3ba1d6fdddf6d63
Jeremy Bennett <jeremylukebennett@comcast.net>
2018-01-21 13:56:53 -0600

    coin-to-cash solution

---
+++ coinsToCash.js
@@ -0,0 +1,30 @@
+let dollarAmount = 0;
+
+const piggyBank = {
+    quarters: 358,
+    dimes: 29,
+    nickels: 93,
+    pennies: 202
+}
+
+
+
+
+for(key in piggyBank) {
+   switch(key) {
+       case "quarters":
+            dollarAmount += piggyBank[key] * .25;
+            break;
+        case "dimes":
+            dollarAmount += piggyBank[key] * .10;
+            break;
+        case "nickels":
+            dollarAmount += piggyBank[key] * .05;
+        case "pennies":
+            dollarAmount += piggyBank[key] * .01;
+   }
+}
+
+
+
+console.log("$" + dollarAmount.toFixed(2));  //Not sure how to restrict to 2 decimal points without converting value to a string :/
